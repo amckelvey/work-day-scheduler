838286f18f1e3b9ff03396a169840163d8b83202
Majelvey <mckelvey.andy@gmail.com>
2022-05-19 08:59:20 -0400

    added comments to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
+// Establish a baseline for today.
 var today = moment();
+// format the date and place it in the header.
 $("#currentDay").text(today.format('dddd, MMM Do'));
 
+// grabbing hour and setting it to timeNow for coming for loop.
 var timeNow = moment().hour();
 
+// traverse the tree of children of the .container class and add bg colors based on time of day
+// i + 8 should start us at 8am and allows for quick conversion to military time.
 for (var i = 0; i < 10; i++) {
     if (timeNow == (i + 8)) {
         $(".container").children().eq(i).children().eq(1).addClass("present");
@@ -15,11 +20,16 @@ for (var i = 0; i < 10; i++) {
 }
 
 $(".saveBtn").on("click", function () {
+    // grab the sibling of .saveBtn, the .description textarea, and set it equal to var text
     var text = $(this).siblings(".description").val();
+    // grab the parent of .saveBtn and its id attribute and set it equal to time
     var time = $(this).parent().attr("id");
+    // setItems in local storage
     localStorage.setItem(time, text);
 })
 
+// Tried this as a for loop but couldn't figure out how to iterate through an index that might not have all the descriptions filled.
+// retrive each description from local storage as needed.
 $("#hour8 .description").val(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
